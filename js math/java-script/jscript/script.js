3ee2cdf128d7572ea45a3d632e85abc7ccd7ae30
rammsanker <109334253+rammsanker54@users.noreply.github.com>
2022-09-24 11:50:58 +0530

    Add files via upload

---
+++ js math/java-script/jscript/script.js
@@ -0,0 +1,37 @@
+//absolute//
+Math.abs(-99);
+console.log(Math.abs(-99));
+//ceil//
+Math.ceil(8.2);
+console.log(Math.ceil(8.2));
+//floor//
+Math.floor(8.9);
+console.log(Math.floor(8.9));
+//random//
+Math.random();
+console.log(Math.random());
+//round//
+Math.round(8.2);
+console.log(Math.round(8.2));
+//power value//
+Math.pow(8,9);
+console.log(Math.pow(8,2));
+//square root//
+Math.sqrt(169);
+console.log(Math.sqrt(169));
+//maxim//
+Math.max(8,89,72);
+console.log(Math.max(8,89,72));
+//minimum//
+Math.min(5,9,2);
+console.log(Math.min(5,9,2));
+//trignometry//
+Math.sin();
+console.log();
+//cube//
+Math.cbrt(27);
+console.log(Math.cbrt(1000));
+//logical //
+var m=30;
+var n=39;
+var l=;
